af9b78a05456316c1e4e68e3c78de25c0767818b
Frantz Cordon <fcordon@z2r5p3.le-101.fr>
2019-04-04 15:19:24 +0200

    ok

--- lf_converter.c
+++ lf_converter.c
@@ -1,13 +1,32 @@
-#define ERROR	1
-#define UNIX	0xf0
-#define WINDOWS	0x0f
+/*
+ * compiler avec -DWINDOWS_OS pour windows et prier pour que ca marche...
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ERROR		1
+#define UNIX		0xf0
+#define WINDOWS		0x0f
+#define BACKUP_DIR	"backup_directory_0xffe8001c__zsj"
+
+#ifdef WINDOWS_OS
+	# define DIR_SEP		'\\'
+#else
+	# define DIR_SEP		'/'
+#endif
 
 int		print_help(char *prog)
 {
 	printf("%s [option] file ...\n\n"
-			"--windows || -w\n"
-			"--unix || -u\n\n",
+			"--windows, -w\n"
+			"--unix, -u\n\n",
 			prog);
+	return (0);
 }
 
 int		get_option(char *s)
@@ -26,28 +45,52 @@ int		get_option(char *s)
 	return (1);
 }
 
-void	replace_all_lf(char	*content, int *size, int opt)
+int		replace_all_lf(char	*content, char *new_content, int opt)
 {
 	char	c = (opt == UNIX) ? '\r' : '\n';
 	char	*lf;
-	int		rem = *size;
+	int		offset = 0;
+	int		len;
 
 	while ((lf = strchr(content, c)))
 	{
 		if (opt == WINDOWS)
 		{
-			memmove(lf + 1, lf, rem);
-			*content = '\r';
-			lf++;
-			*size++;
+			if (lf[-1] == '\r')
+			{
+				strncpy(new_content + offset, content, lf - content + 1);
+				offset += (lf - content) + 1;
+				//fprintf(stderr, "Error: '\\r' detected, stop.\n");
+				//exit(1);
+			}
+			else
+			{
+				strncpy(new_content + offset, content, lf - content);
+				offset += (lf - content) + 2;
+				new_content[offset - 2] = '\r';
+				new_content[offset - 1] = '\n';
+			}
 		}
 		else
 		{
-			memmove(lf, lf + 1, rem);
-			*size--;
+			if (lf[1] != '\n')
+				strncpy(new_content + offset, content, lf - content + 1);
+			else
+				strncpy(new_content + offset, content, lf - content);
+			offset += (lf - content);
 		}
 		content = lf + 1;
 	}
+
+	if (offset == 0)
+	{
+		fprintf(stderr, "Error: no '\\n' detected, stop.\n");
+		exit(1);
+	}
+	len = strlen(content);
+	strncpy(new_content + offset, content, len);
+	offset += len;
+	return (offset);
 }
 
 char	*file_get_contents(char *file, int *readl)
@@ -62,19 +105,34 @@ char	*file_get_contents(char *file, int *readl)
 		fprintf(stderr, "can't open %s\n", file);
 		return (NULL);
 	}
-	lstat(&st, file);
+	lstat(file, &st);
 	if (file_size < st.st_size)
 	{
 		file_size = st.st_size;
 		free(content);
-		content = malloc(file_size);
+		content = malloc(file_size + 1);
 	}
 	read(fd, content, st.st_size);
+	close(fd);
+	content[st.st_size] = '\0';
 	*readl = st.st_size;
 	return (content);
 }
 
-char	*malloc_new_content(char *content, int size, int *new_size, int opt)
+int		count_lf(char *s)
+{
+	char	*tmp;
+	int		count = 0;
+
+	while ((tmp = strstr(s, "\r\n")))
+	{
+		count++;
+		s = tmp + 2;
+	}
+	return (count);
+}
+
+char	*malloc_new_content(char *content, int content_size, int *size, int opt)
 {
 	static char	*new_content = NULL;
 	static int	new_size = 0;
@@ -82,22 +140,55 @@ char	*malloc_new_content(char *content, int size, int *new_size, int opt)
 
 	if (opt == WINDOWS)
 		lf_count = count_lf(content);
-	*new_size = lf_count + size;
-	return (malloc(*new_size));
+	if (new_size < content_size + lf_count)
+	{
+		free(new_content);
+		new_size = lf_count + content_size;
+		*size = new_size;
+		return (malloc(new_size));
+	}
+	*size = new_size;
+	return (new_content);
+}
+
+char	*get_backup_name(const char *name)
+{
+	char		*new;
+	const char	*start = name;
+	static int	uniq_no = 0;
+
+	name += strlen(name) - 1;
+	while (name > start && *name != DIR_SEP)
+		name--;
+	new = malloc(strlen(name) + 11 + strlen(BACKUP_DIR) + 1);
+	sprintf(new, BACKUP_DIR "%c%s_0x%x", DIR_SEP, name, uniq_no);
+	uniq_no++;
+	return (new);
 }
 
 void	replace_lf(int opt, char *file)
 {
 	char	*content;
+	char	*new_content;
 	int		size;
-	char	*new_content = NULL;
-	int		new_size = 0;
-	int		nlf = 0;
+	int		new_size;
+	char	*backup_name;
 	
 	if ((content = file_get_contents(file, &size)) == NULL)
 		return;
 	new_content = malloc_new_content(content, size, &new_size, opt);
-	replace_all_lf(content, size, opt);
+	new_size = replace_all_lf(content, new_content, opt);
+	backup_name = get_backup_name(file);
+	rename(file, backup_name);
+	int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		rename(backup_name, file);
+		fprintf(stderr, "Error 0x01\n");
+		exit(1);
+	}
+	write(fd, new_content, new_size);
+	close(fd);
 }
 
 int		main(int ac, char *av[])
@@ -108,8 +199,10 @@ int		main(int ac, char *av[])
 		return (print_help(av[0]));
 	if ((opt = get_option(av[1])) == ERROR)
 		return (ERROR);
+	mkdir(BACKUP_DIR, 0777);
 	for (av += 2; *av; av++)
 	{
 		replace_lf(opt, *av);
 	}
+	return (0);
 }
